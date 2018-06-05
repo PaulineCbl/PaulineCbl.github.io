9250f47ede0ba16a9f3df127bae83a6ec864599d
Pauline Carbonnel <paulinecarbonnel@vagelos-ve702-4579.apng.wlan.private.upenn.edu>
2018-06-05 11:57:01 -0400

    added food

--- python/my_info.py
+++ python/my_info.py
@@ -1,1 +1,3 @@
 my_info = {"first name" : "Pauline" , "last_name" : "Carbonnel" , "hometown" : "Carcassonne" , "favorite food" : "cornbread"}
+my_info["good food"]= "coffee"
+my_info["favorite food"]= "candies"
