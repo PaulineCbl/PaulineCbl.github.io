0722dc509f6eb4519775bea88a6c015b31be39b6
Pauline Carbonnel <paulinecarbonnel@vagelos-ve702-4579.apng.wlan.private.upenn.edu>
2018-06-06 11:17:08 -0400

    exercice with 19 or 20 birth dates

---
+++ python/authors_birthdates.py
@@ -0,0 +1,12 @@
+birth_dates = {"Proust": 1871, "Claude Simon": 1913, "Jaccottet": 1925, "Flaubert": 1821}
+
+nineteenth_count = 0
+twentieth_count = 0
+
+for person, if date in birth_dates.items():
+    if date <1900:
+        nineteenth_count += 1
+    else:
+        twentieth_count += 1
+
+            
